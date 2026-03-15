e0111e8a7dcf6fd0bcf5efbf1c6cb227f4fc83f0
herrdextro <n.dijkstra.inf23@stud.bbbaden.ch>
2026-03-15 22:26:04 +0100

    added logic for refresh tokens, untested

---
+++ cloud.api/Models/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace cloud.api.Models
+{
+    public class RefreshToken
+    {
+        public string UserId { get; set; }
+        public DateOnly ExpiryDate { get; set; }
+
+        public string Token { get; set; }
+    }
+}
---
+++ cloud.api/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using cloud.api.Models;
+using cloud.api.Settings;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+
+
+namespace cloud.api.Services
+{
+    public class AuthService
+    {
+        
+        IMongoCollection<RefreshToken> _refreshTokens;
+        public AuthService(IMongoClient mongoClient, IOptions<MongoSettings> settings) 
+        { 
+            var database = settings.Value.UserDatabase;
+            _refreshTokens = mongoClient.GetDatabase(database).GetCollection<RefreshToken>("refreshTokens"); //need to test this
+        }
+        public async Task<RefreshToken> GenerateRefreshToken(User user)
+        {
+            var randNum = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randNum);
+            string refreshToken = Convert.ToBase64String(randNum);
+            return new RefreshToken
+            {
+                UserId = user.Id, 
+                ExpiryDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(7)), 
+                Token = refreshToken
+            };
+        }
+        public async Task StoreRefreshToken(RefreshToken token)
+        {
+            await _refreshTokens.InsertOneAsync(token);
+        }
+
+    }
+}
